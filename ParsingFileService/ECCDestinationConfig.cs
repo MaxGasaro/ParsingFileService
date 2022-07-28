eb4d5f837e51e8c71b1f672b10a5e6301697aec2
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-07-28 11:10:59 +0200

    Add project files.

---
+++ ParsingFileService/ECCDestinationConfig.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using SAP.Middleware.Connector;
+
+namespace ParsingFileService
+{
+    public class ECCDestinationConfig : IDestinationConfiguration
+    {
+        public event RfcDestinationManager.ConfigurationChangeHandler ConfigurationChanged;
+
+        public bool ChangeEventsSupported()
+        {
+            return true;
+        }
+
+        public RfcConfigParameters GetParameters(string destinationName)
+        {
+            RfcConfigParameters parms = new RfcConfigParameters();
+
+                if (destinationName.Equals("mySAPdestination"))
+            {
+                parms.Add(RfcConfigParameters.AppServerHost, "IPAddress");
+                parms.Add(RfcConfigParameters.SystemNumber, "00");
+                parms.Add(RfcConfigParameters.SystemID, "ID");
+                parms.Add(RfcConfigParameters.User, "Username");
+                parms.Add(RfcConfigParameters.Password, "Password");
+                parms.Add(RfcConfigParameters.RepositoryPassword, "Password");
+                parms.Add(RfcConfigParameters.Client, "100");
+                parms.Add(RfcConfigParameters.Language, "EN");
+                parms.Add(RfcConfigParameters.PoolSize, "5");
+            }
+            return parms;
+        }
+
+        internal static void CallingSAP()
+        {
+            ECCDestinationConfig cfg = null;
+            RfcDestination dest = null;
+
+            try
+            {
+                cfg = new ECCDestinationConfig();
+                RfcDestinationManager.RegisterDestinationConfiguration(cfg);
+
+                dest = RfcDestinationManager.GetDestination("mySAPdestination");
+
+                RfcRepository repo = dest.Repository;
+
+                IRfcFunction fnpush = repo.CreateFunction("ZFUNCTION");
+                // Send Data with RFC Structure
+                IRfcStructure data = fnpush.GetStructure("IM_STRUCTURE");
+                data.SetValue("ITEM1", "VALUE1");
+                data.SetValue("ITEM2", "VALUE2");
+                data.SetValue("ITEM3", "VALUE3");
+                data.SetValue("ITEM4", "VALUE4");
+
+                fnpush.SetValue("IM_STRUCTURE", data);
+
+                // Send data with RFC table
+                IRfcTable dataTbl = fnpush.GetTable("IM_TABLE");
+
+                //foreach(var item in ListItem)
+                {
+                    dataTbl.Append();
+                    dataTbl.SetValue("ITEM1", "VALUE1");
+                    dataTbl.SetValue("ITEM2", "VALUE2");
+                    dataTbl.SetValue("ITEM3", "VALUE3");
+                    dataTbl.SetValue("ITEM4", "VALUE4");
+                }
+
+                fnpush.Invoke(dest);
+
+                var exObject = fnpush.GetObject("EX_OBJECT");
+                IRfcStructure exStructure = fnpush.GetStructure("EX_STRUCTURE");
+
+                RfcSessionManager.EndContext(dest);
+
+                RfcDestinationManager.UnregisterDestinationConfiguration(cfg);
+            }
+            catch (Exception ex)
+            {
+                RfcSessionManager.EndContext(dest);
+
+                RfcDestinationManager.UnregisterDestinationConfiguration(cfg);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ ParsingFileService/ParsingFileService.cs
@@ -0,0 +1,99 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace ParsingFileService
+{
+    public partial class ParsingFileService : ServiceBase
+    {
+
+        string path = @"D:\xml\LOIBOM_18622669.xml";
+        public ParsingFileService()
+        {
+            InitializeComponent();
+        }
+
+        //protected override void OnStart(string[] args)
+        //{
+        //}
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(ParsingFileService));
+        //private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // funzione per parsificare file xml utilizzando un xslt
+        public void ParsingByXslt()
+        {
+            string path = @"D:\idoc\IDOC_202207\IDOC_14480344_20220705193956173.xml";
+            string destPath = @"D:\idoc\IDOC_PARS_XSLT\JISORD_IDOC_TRANSFORM.html";
+            //istanzio un nuovo oggetto xsltransform
+            XslTransform xslt = new XslTransform();
+
+            //carico il file stilistico xslt
+            xslt.Load(@"D:\idoc\IDOC_202207\XSLT\MagnetiMarelli-Order-Job67.xslt");
+
+            //carico il file xml da parsificare 
+            //XPathDocument mydata = new XPathDocument(@"D:\idoc\IDOC_202207\MATMAS_IDOC_190115807_20220517082638316.xml");
+
+            //creo un output tramite log4net
+            //log.Debug(result);
+            
+            //creo un output tramite xmltextwriter
+            //XmlWriter writer = XmlWriter.Create(destPath);
+
+            //trasformo il file e mando l'output alla console
+            //xslt.Transform(mydata, null, writer, null);
+
+            //utilizzo il metodo transform ma passandogli due semplici url
+            xslt.Transform(path,destPath);
+
+        }
+
+        
+            
+            
+        // funzione ricorsiva per parsificare file xml estrapolando i soli valori
+        public void ParsingDoc(XElement e)
+        {
+            foreach (XElement element in e.Elements())
+            {
+                if (element.Elements().Count() == 0)
+                {
+                    log.Debug(element.Value);
+                }
+                else
+                {
+                    ParsingDoc(element);
+                }
+            }
+        }
+        
+        public void Start()
+        {
+            log.Debug(path);
+
+            XDocument doc = XDocument.Load(path);
+
+            foreach(XElement e in doc.Elements())
+            {
+                this.ParsingDoc(e);
+            }
+
+            this.ParsingByXslt();
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
